ba75a269e63896f6ea779163915fc2fadcb94bf7
okapies <okapies@gmail.com>
2014-02-08 12:35:38 +0900

    bump scala version to 2.10.3 and spray-can to 1.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build extends sbt.Build{
       Defaults.defaultSettings ++ Seq(
       organization  := "com.example",
       version       := "0.1",
-      scalaVersion  := "2.10.2",
+      scalaVersion  := "2.10.3",
 
       resolvers ++= Seq(
         "typesafe repo"      at "http://repo.typesafe.com/typesafe/releases/",
@@ -18,7 +18,7 @@ object Build extends sbt.Build{
         "spray nightly"      at "http://nightlies.spray.io/"
       ),
       libraryDependencies ++= Seq(
-        "io.spray"            %   "spray-can"     % "1.2-RC3",
+        "io.spray"            %   "spray-can"     % "1.2.0",
         "com.typesafe.akka"   %%  "akka-actor"    % "2.2.3",
         "com.typesafe.akka"   %%  "akka-testkit"  % "2.2.3" % "test",
         "org.scalatest"       % "scalatest_2.10" % "2.0" % "test"
